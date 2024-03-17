d4e7f5103c53a4fc09075b8eff1997f7b58e8ddd
Ramz <84850359+Ramz001@users.noreply.github.com>
2024-03-17 21:38:36 +0500

    cors issue fix 1

--- server/server.js
+++ server/server.js
@@ -16,6 +16,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 app.locals.OTP = null
 
 app.use("/api/workouts", workoutRoutes);
