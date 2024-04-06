142527d69821e9d17fee6c33380ae716c720bdd7
Ramz <84850359+Ramz001@users.noreply.github.com>
2024-04-06 19:33:05 +0500

    fonts removed to optimize performance

--- client/src/routes/Home/Home.js
+++ client/src/routes/Home/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <main
       className="mx-auto max-w-[1920px] bg-slate-50 px-4 py-6 sm:px-6 
-    sm:py-8 md:min-h-max  md:px-16 2xl:min-h-[92vh] dark:bg-slate-800"
+    sm:py-8 md:min-h-max font-sans md:px-16 2xl:min-h-[92vh] dark:bg-slate-800"
     >
       <WorkoutsContainer />
     </main>
