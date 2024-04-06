1a2d4b9a91b0b99f06d8a2aaf264b8d19f09072f
Ramz <84850359+Ramz001@users.noreply.github.com>
2024-04-06 10:03:20 +0500

    Update App.js
    Fixed app route closure

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ function App() {
     >
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}
+        <Route path="/" element={<Home />}/>
         <Route
           path="/login"
           element={isSignedIn ? <Navigate to="/" /> : <Login />}
