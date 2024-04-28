a352270f88b25a15593ac19774934ca5de6892c8
Ramz <84850359+Ramz001@users.noreply.github.com>
2024-04-28 21:43:56 +0500

    server clustering enabled

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const localVariables = require('./middleware/localVariables')
 require("dotenv").config({ path: "./src/configs/.env" })
+const cluster = require("cluster") 
 
 const corsOptions = {
   origin: [`http://localhost:${process.env.PORT}` ,"https://mr-workout-buddy.vercel.app/**"], // frontend URI (ReactJS)
@@ -20,13 +21,19 @@ app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
 app.use(localVariables)
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    app.listen(process.env.PORT, (req, res) => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  })
-  .catch((error) => console.log(error));
+if(cluster.isMaster){
+  cluster.fork()
+  cluster.fork()
+} else{
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      app.listen(process.env.PORT, (req, res) => {
+        console.log(`App is listening on port ${process.env.PORT} ${process.pid}`);
+      });
+    })
+    .catch((error) => console.log(error));
+}
+
 
 module.exports = app
